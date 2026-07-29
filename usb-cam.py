266a5c89559c6da0086377bd71362f65a211da2d
Leo Lee <ckocklly@gmail.com>
2026-07-29 15:36:19 -0500

    modified camera & add img poke-wo-resin

--- usb-cam.py
+++ usb-cam.py
@@ -1,5 +1,5 @@
+import os
 import cv2
-import sys
 
 # Find the number corresponding to the USB CAM
 s = 2
@@ -7,6 +7,12 @@ s = 2
 source = cv2.VideoCapture(s)
 source.set(cv2.CAP_PROP_FRAME_WIDTH, 3840)
 source.set(cv2.CAP_PROP_FRAME_HEIGHT, 2160)
+
+actual_width = source.get(cv2.CAP_PROP_FRAME_WIDTH)
+actual_height = source.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+print(f"Resolution: {actual_width} x {actual_height}")
+print(f"Press Esc to close the window and capture the image.")
 
 win_name = 'Camera Preview'
 cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
@@ -16,6 +22,13 @@ while cv2.waitKey(1) != 27: # Escape
     if not has_frame:
         break
     cv2.imshow(win_name, frame)
+
+filename = "capture.png"
+dst_folder = "img"
+ret, frame = source.read()
+if ret:
+    cv2.imwrite(os.path.join(dst_folder, filename), frame)
+    print(f"Saved \"{filename}\" to folder \"{dst_folder}\".")
 
 source.release()
 cv2.destroyWindow(win_name)
